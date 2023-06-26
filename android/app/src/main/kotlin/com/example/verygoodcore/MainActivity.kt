b203ee502e1a4e128be14644bd035940dd1679a6
Juriba <Juribaalsiari@gmail.com>
2023-06-26 15:49:30 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.cerati
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:cerati/app/app.dart';
+import 'package:cerati/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
