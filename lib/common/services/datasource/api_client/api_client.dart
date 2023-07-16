dd818c80b5422b4e02889c404c5d5aca4038c155
Juriba <Juribaalsiari@gmail.com>
2023-07-16 20:44:13 +0300

    save changes

--- lib/common/services/datasource/api_client/api_client.dart
+++ lib/common/services/datasource/api_client/api_client.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: inference_failure_on_function_invocation
+// ignore_for_file: inference_failure_on_function_invocation, avoid_dynamic_calls
 
 import 'package:cerati/common/constants/api_contstants.dart';
 import 'package:cerati/common/error_handling/parse_http_errors.dart';
@@ -20,10 +20,11 @@ class ApiClient implements ApiClientInterface {
       final response = await _networkManager.dio.post(_apiConstants.auth.login,data: {
         loginRequestModel.toJson()
       },);
-      if(response.statusCode != 200){
+      final body = response.data['asd'] as Map<String,dynamic>;
+      if(response.statusCode != 200 || body['status']  == 'error'){
         throw parseHttpErrors(DioException(requestOptions: response.requestOptions));
       }
-      return LoginResponseModel.fromJson(response.data as Map<String,dynamic>);
+      return LoginResponseModel.fromJson(body);
     }on DioException catch (error){
       throw parseHttpErrors(error);
     }
