35e6d606b70933a846595f1f7498695884168444
Juriba <Juribaalsiari@gmail.com>
2023-07-18 23:50:07 +0300

    remove .env

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'package:cerati/app/app.dart';
 import 'package:cerati/bootstrap.dart';
 import 'package:cerati/repositories_injection.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:logger/logger.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
 
   await bootstrap(() {
     return multiRepositoryProvider(
--- lib/repositories_injection.dart
+++ lib/repositories_injection.dart
@@ -5,29 +5,31 @@ import 'package:cerati/features/login/repository/login_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 MultiRepositoryProvider multiRepositoryProvider({required Widget child}) {
   return MultiRepositoryProvider(
     providers: [
       RepositoryProvider<NetworkManager>(
-        create: (networkManagerContext) => NetworkManager(
-          baseUrl: dotenv.get('DEV_BASE_URL'),
-          dio: Dio(),
-        ),
+        create: (networkManagerContext) =>
+            NetworkManager(
+              baseUrl: 'http://localhost:8000/api/',
+              dio: Dio(),
+            ),
       ),
       RepositoryProvider<ApiClient>(
-        create: (apiClientContext) => ApiClient(
-          apiClientContext.read<NetworkManager>(),
-        ),
+        create: (apiClientContext) =>
+            ApiClient(
+              apiClientContext.read<NetworkManager>(),
+            ),
       ),
       RepositoryProvider<SecureLocalStorage>(
         create: (secureLocalStorageContext) => SecureLocalStorage(),
       ),
       RepositoryProvider<LoginRepository>(
-        create: (loginRepositoryContext) => LoginRepository(
-          loginRepositoryContext.read<ApiClient>(),
-        ),
+        create: (loginRepositoryContext) =>
+            LoginRepository(
+              loginRepositoryContext.read<ApiClient>(),
+            ),
       )
     ],
     child: child,
