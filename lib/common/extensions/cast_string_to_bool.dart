c41c456b510eebcf60db5e2562a69d3e6b5973b3
Juriba <Juribaalsiari@gmail.com>
2023-07-27 01:10:30 +0300

    CER-43 setup theme data

--- lib/common/extensions/cast_string_to_bool.dart
+++ lib/common/extensions/cast_string_to_bool.dart
@@ -1,11 +1,9 @@
 extension CastStringToBool on String {
   bool toBoolean() {
-    if (this.toLowerCase() == 'true' || this.toLowerCase() == '1') {
+    if (toLowerCase() == 'true' || toLowerCase() == '1') {
       return true;
     } else {
-      return (this.toLowerCase() == 'false' || this.toLowerCase() == '0')
-          ? false
-          : throw UnsupportedError('Unsupported String');
+      return (toLowerCase() == 'false' || toLowerCase() == '0') ? false : throw UnsupportedError('Unsupported String');
     }
   }
 }
--- test/common_test/services/language_manager_test/language_manager_test.dart
+++ test/common_test/services/language_manager_test/language_manager_test.dart
@@ -17,24 +17,57 @@ void main() {
       languageManager = UserSettingManager(localStorage);
     });
 
-    test('changeLanguage writes locale.languageCode to storage', () async {
+    test('setLocale writes locale.languageCode to storage', () async {
       // Arrange
       const locale = Locale('en');
-      when(() => localStorage.write<String>(key: any(named: 'key'), data: any(named: 'data')))
-          .thenAnswer((_) async => true);
+      when(
+        () => localStorage.write<String>(
+          key: any(named: 'key'),
+          data: any(named: 'data'),
+        ),
+      ).thenAnswer((_) async => true);
 
       // Act
       await languageManager.setLocale(locale: locale);
 
       // Assert
-      verify(() => localStorage.write<String>(key: StorageConstants.languageKey, data: locale.languageCode)).called(1);
+      verify(
+        () => localStorage.write<String>(key: StorageConstants.languageKey, data: locale.languageCode),
+      ).called(1);
+    });
+
+    test('setThemeMode writes isDark value to storage', () async {
+      // Arrange
+      const isDark = true;
+      when(
+        () => localStorage.write<String>(
+          key: any(named: 'key'),
+          data: any(named: 'data'),
+        ),
+      ).thenAnswer((_) async => true);
+
+      // Act
+      await languageManager.setThemeMode(isDark: isDark);
+
+      // Assert
+      verify(
+        () => localStorage.write<String>(key: StorageConstants.themeModeKey, data: (!isDark).toString()),
+      ).called(1);
     });
 
     test('getLocal returns correct locale from storage', () async {
       // Arrange
       const storedLocaleCode = 'en';
-      when(() => localStorage.hasData(key: any(named: 'key'))).thenReturn(true);
-      when(() => localStorage.read<String>(key: any(named: 'key'))).thenAnswer((_) async => storedLocaleCode);
+      when(
+        () => localStorage.hasData(
+          key: any(named: 'key'),
+        ),
+      ).thenReturn(true);
+      when(
+        () => localStorage.read<String>(
+          key: any(named: 'key'),
+        ),
+      ).thenAnswer((_) async => storedLocaleCode);
 
       // Act
       final locale = await languageManager.getLocal();
@@ -43,8 +76,41 @@ void main() {
       expect(locale, isNotNull);
       expect(locale.languageCode, equals(storedLocaleCode));
 
-      verify(() => localStorage.hasData(key: StorageConstants.languageKey)).called(1);
-      verify(() => localStorage.read<String>(key: StorageConstants.languageKey)).called(1);
+      verify(
+        () => localStorage.hasData(key: StorageConstants.languageKey),
+      ).called(1);
+      verify(
+        () => localStorage.read<String>(key: StorageConstants.languageKey),
+      ).called(1);
+    });
+
+    test('getThemeMode returns correct isDark value from storage', () async {
+      // Arrange
+      const storedIsDark = true;
+      when(
+        () => localStorage.hasData(
+          key: any(named: 'key'),
+        ),
+      ).thenReturn(true);
+      when(
+        () => localStorage.read<String>(
+          key: any(named: 'key'),
+        ),
+      ).thenAnswer((_) async => storedIsDark.toString());
+
+      // Act
+      final isDark = await languageManager.getThemeMode();
+
+      // Assert
+      expect(isDark, isNotNull);
+      expect(isDark, equals(storedIsDark.toString()));
+
+      verify(
+        () => localStorage.hasData(key: StorageConstants.themeModeKey),
+      ).called(1);
+      verify(
+        () => localStorage.read<String>(key: StorageConstants.themeModeKey),
+      ).called(1);
     });
   });
 }
