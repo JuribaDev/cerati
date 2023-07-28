212ce54bcdf594da5986e9542dc5be99fcad1c2a
Juriba <Juribaalsiari@gmail.com>
2023-07-28 21:42:50 +0300

    CER-48 add register repository

---
+++ test/helpers/mocks.dart
@@ -0,0 +1,5 @@
+// Mock ApiClient
+import 'package:cerati/common/services/datasource/api_client/api_client.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockApiClient extends Mock implements ApiClient {}
