01dccfc11751ccb3ef1ee6b76c2cacea8b16656b
Juriba <Juribaalsiari@gmail.com>
2023-08-15 18:12:56 +0300

    CER-72 add route for user account

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,14 @@
 import 'package:cerati/common/constants/routes_constants.dart';
 import 'package:cerati/features/login/screen/login_screen.dart';
 import 'package:cerati/features/register/screen/register_screen.dart';
+import 'package:cerati/features/user_account/screen/user_account_screen.dart';
 import 'package:go_router/go_router.dart';
 
 GoRouter router([String? initialLocation]) => GoRouter(
-      initialLocation: initialLocation ?? RoutesConstants.register,
+      initialLocation: initialLocation ?? RoutesConstants.login,
       routes: [
         GoRoute(path: RoutesConstants.login, builder: (context, state) => const LoginScreen()),
         GoRoute(path: RoutesConstants.register, builder: (context, state) => const RegisterScreen()),
+        GoRoute(path: RoutesConstants.userAccount, builder: (context, state) => const UserAccountScreen()),
       ],
     );
